c0170528ae206402ef1a3462a5b36b32482398ce
AdamNewmanBooth <164922100+AdamNewmanBooth@users.noreply.github.com>
2024-04-06 15:45:57 +0000

    Commit4

--- dice.rb
+++ dice.rb
@@ -1,4 +1,6 @@
 # /dice.rb
+require "better_errors"
+require "binding_of_caller"
 require "sinatra"
 
 get("/") do
@@ -7,4 +9,21 @@ end
 
 get("/zebra") do
   "We must add a route for each path we want to support"
+end
+
+require "sinatra/reloader"
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
 end
