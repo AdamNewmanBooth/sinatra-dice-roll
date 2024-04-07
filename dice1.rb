8f0dafbae4d72949f65ee2e2dcc1cc106b8c0b09
AdamNewmanBooth <164922100+AdamNewmanBooth@users.noreply.github.com>
2024-04-07 23:28:01 +0000

    Commit8

--- dice1.rb
+++ dice1.rb
@@ -3,9 +3,7 @@
 require "sinatra"
 gem "better_errors"
 gem "binding_of_caller"
-use(BetterErrors::Middleware)
-BetterErrors.application_root = __dir__
-BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
 
 pp "hey!"
 
@@ -23,3 +21,24 @@ get("/girafe") do
   "Hopefully this shows up without having to restart the server 🤞🏾"
 end
 
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d10</h1>
+   <p>#{outcome}</p>"
+end
