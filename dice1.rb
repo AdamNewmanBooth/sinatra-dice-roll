624dae62823f9ea87346b999893f5713edd22d7c
AdamNewmanBooth <164922100+AdamNewmanBooth@users.noreply.github.com>
2024-04-07 23:19:44 +0000

    Bundles

--- dice1.rb
+++ dice1.rb
@@ -1,6 +1,11 @@
 # /dice.rb
 
 require "sinatra"
+gem "better_errors"
+gem "binding_of_caller"
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 pp "hey!"
 
